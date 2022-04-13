390d5b5fbc47cba915c964d89b55ba9e1c66db98
Charles Ngo <charlesngo37@gmail.com>
2022-04-12 20:20:46 -0700

    Refactored code

--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,4 @@
 const spinAnimation = ['\r|.....', '\r/.....', '\r-.....', '\r\\.....'];
-const totalSpins = 40;
-let animationStep = 0;
-for (let i = 1; i <= totalSpins; i++) {
-  setTimeout(() => {
-    process.stdout.write(spinAnimation[animationStep]);
-    animationStep++;
-    if (animationStep > 3) animationStep = 0;
-  }, 400 * i);
+for (let i = 1; i <= 40; i++) {
+  setTimeout(() => process.stdout.write(spinAnimation[i % 4]), 400 * i);
 }
